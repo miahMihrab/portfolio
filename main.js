bd2910b90876a617fd61cfafb8b9dcad1c42c6d0
Mihrab Miah <mihrabmiah13@gmail.com>
2019-09-18 16:44:06 +0600

    design changed + resume edited

--- main.js
+++ main.js
@@ -1,81 +1,139 @@
 //console.log(window.width)
 setInterval(() => {
-
-    if (document.querySelector('#about').clientWidth < 250) {
-        document.querySelector('.structure').style.width = "50px";
-        document.querySelector('#about p').style.fontsize = "12px";
-        console.log(document.querySelector('.structure').clientWidth)
-    }
-}, 10)
-const nav = document.querySelector('.nav-items');
-const menubar = document.querySelector('.menu-bar');
-const dev_logo = document.querySelector('.logo');
-const about_modal = document.querySelector('.modal_wrapper')
-const close = document.querySelector('.close');
-const button = document.querySelector('button');
-
+  if (document.querySelector("#about").clientWidth < 250) {
+    document.querySelector(".structure").style.width = "50px";
+    document.querySelector("#about p").style.fontsize = "12px";
+    console.log(document.querySelector(".structure").clientWidth);
+  }
+}, 10);
+const nav = document.querySelector(".nav-items");
+const menubar = document.querySelector(".menu-bar");
+const dev_logo = document.querySelector(".logo");
+const about_modal = document.querySelector(".modal_wrapper");
+const close = document.querySelector(".close");
+const button = document.querySelector("button");
 
 function showHide(value) {
-    value = (value.toLowerCase()).trim();
-    //console.log(value)
-    const about = document.querySelector('#about');
-    const works = document.querySelector('#works');
-    if (value === 'about') {
-        if (Array.from(about.classList).includes('hide')) {
-            about.classList.remove('hide');
-            about.classList.add('show');
-            if (Array.from(works.classList).includes('show')) {
-                works.classList.remove('show');
-                works.classList.add('hide');
-            }
-        } else {
-            about.classList.add('hide');
-            about.classList.remove('show')
-        }
+  value = value.toLowerCase().trim();
+  console.log(value);
+  const about = document.querySelector("#about");
+  const works = document.querySelector("#works");
+  const resume = document.querySelector("#resume");
+  const blog = document.querySelector("#blog");
+  if (value === "about") {
+    if (Array.from(about.classList).includes("hide")) {
+      about.classList.remove("hide");
+      about.classList.add("show");
+      if (Array.from(works.classList).includes("show")) {
+        works.classList.remove("show");
+        works.classList.add("hide");
+      }
+      if (Array.from(resume.classList).includes("show")) {
+        resume.classList.remove("show");
+        resume.classList.add("hide");
+      }
+      if (Array.from(blog.classList).includes("show")) {
+        blog.classList.remove("show");
+        blog.classList.add("hide");
+      }
+    } else {
+      about.classList.add("hide");
+      about.classList.remove("show");
     }
+  }
 
-    if (value === 'works') {
-        if (Array.from(works.classList).includes('hide')) {
-            works.classList.remove('hide');
-            works.classList.add('show');
-            if (Array.from(about.classList).includes('show')) {
-                about.classList.remove('show');
-                about.classList.add('hide');
-            }
-        } else {
-            works.classList.add('hide');
-            works.classList.remove('show')
-        }
+  if (value === "resume") {
+    if (Array.from(resume.classList).includes("hide")) {
+      resume.classList.remove("hide");
+      resume.classList.add("show");
+      if (Array.from(about.classList).includes("show")) {
+        about.classList.remove("show");
+        about.classList.add("hide");
+      }
+      if (Array.from(works.classList).includes("show")) {
+        works.classList.remove("show");
+        works.classList.add("hide");
+      }
+      if (Array.from(blog.classList).includes("show")) {
+        blog.classList.remove("show");
+        blog.classList.add("hide");
+      }
+    } else {
+      resume.classList.add("hide");
+      resume.classList.remove("show");
     }
-    if (value === 'contact') {
-        about_modal.style.display = 'block';
+  }
+
+  if (value === "works") {
+    if (Array.from(works.classList).includes("hide")) {
+      works.classList.remove("hide");
+      works.classList.add("show");
+      if (Array.from(about.classList).includes("show")) {
+        about.classList.remove("show");
+        about.classList.add("hide");
+      }
+      if (Array.from(resume.classList).includes("show")) {
+        resume.classList.remove("show");
+        resume.classList.add("hide");
+      }
+      if (Array.from(blog.classList).includes("show")) {
+        blog.classList.remove("show");
+        blog.classList.add("hide");
+      }
+    } else {
+      works.classList.add("hide");
+      works.classList.remove("show");
     }
+  }
+  if (value === "blog") {
+    if (Array.from(blog.classList).includes("hide")) {
+      blog.classList.remove("hide");
+      blog.classList.add("show");
+      if (Array.from(about.classList).includes("show")) {
+        about.classList.remove("show");
+        about.classList.add("hide");
+      }
+      if (Array.from(resume.classList).includes("show")) {
+        resume.classList.remove("show");
+        resume.classList.add("hide");
+      }
+      if (Array.from(works.classList).includes("show")) {
+        works.classList.remove("show");
+        works.classList.add("hide");
+      }
+    } else {
+      blog.classList.add("hide");
+      blog.classList.remove("show");
+    }
+  }
+
+  if (value === "contact") {
+    about_modal.style.display = "block";
+  }
 }
-const aside = document.querySelector('aside')
-aside.addEventListener('click', e => {
-    const about = document.querySelector('#about');
-    const works = document.querySelector('#works');
-    try {
-        const nxtSibling = document.getElementById(e.target.id).nextElementSibling;
-        showHide(nxtSibling.textContent);
-        //console.log(nxtSibling.textContent)
-    } catch {
-        showHide(e.target.textContent);
-        //console.log(e.target.textContent)
-    }
-
-})
-
+const aside = document.querySelector("aside");
+aside.addEventListener("click", e => {
+  const about = document.querySelector("#about");
+  const works = document.querySelector("#works");
+  try {
+    const nxtSibling = document.getElementById(e.target.id).nextElementSibling;
+    showHide(nxtSibling.textContent);
+    //console.log(nxtSibling.textContent)
+  } catch {
+    showHide(e.target.textContent);
+    //console.log(e.target.textContent)
+  }
+});
 
 // dev_logo.addEventListener('click', e => {
 //     console.log(about_modal)
 //     about_modal.style.display = 'block';
 // })
-close.addEventListener('click', e => {
-    about_modal.style.display = 'none';
-})
-about_modal.addEventListener('click', e => {
-    //console.log(e.target)
-    if (Array.from(e.target.classList).includes('modal_wrapper'))
-        about_modal.style.display = 'none';
-})
+close.addEventListener("click", e => {
+  about_modal.style.display = "none";
+});
+about_modal.addEventListener("click", e => {
+  //console.log(e.target)
+  if (Array.from(e.target.classList).includes("modal_wrapper"))
+    about_modal.style.display = "none";
+});
